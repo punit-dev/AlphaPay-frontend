b1167e4b463fbf7bddd438121017783295a4b3bc
punit-dev <punitpoddar2310@gmail.com>
2026-02-24 16:37:54 +0530

    add global error handler

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from "./App.jsx";
 import { BrowserRouter as Router } from "react-router";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorScreen from "./screens/ErrorScreen";
 
 import { registerSW } from "virtual:pwa-register";
 
@@ -17,12 +19,22 @@ const updateSW = registerSW({
   },
 });
 
+function ErrorFallback({ error, resetErrorBoundary }) {
+  return <ErrorScreen error={error?.message} onRetry={resetErrorBoundary} />;
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Router>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </Router>
+    <ErrorBoundary
+      fallback={ErrorFallback}
+      onReset={() => {
+        window.location.reload();
+      }}>
+      <Router>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </Router>
+    </ErrorBoundary>
   </StrictMode>,
 );
